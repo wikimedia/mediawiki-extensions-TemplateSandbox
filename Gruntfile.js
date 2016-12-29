478860e33bb93bf075c205854cf229c8de886c66
Ed Sanders <esanders@wikimedia.org>
2016-12-29 15:08:35 +0000

    Introduce eslint and bump dependencies
    Change-Id: I1e6a8e606892e0bec033f62b59afb98fbb98aa5a

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,18 @@
-/*jshint node:true */
+/* eslint-env node */
 module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 
-	var conf = grunt.file.readJSON( 'extension.json' );
 	grunt.initConfig( {
+		eslint: {
+			all: [
+				'**/*.js',
+				'!node_modules/**'
+			]
+		},
 		banana: conf.MessagesDirs,
 		jsonlint: {
 			all: [
@@ -14,6 +22,6 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'jsonlint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
--- modules/ext.TemplateSandbox.js
+++ modules/ext.TemplateSandbox.js
@@ -1,11 +1,13 @@
 /** extensions/TemplateSandbox/modules/ext.TemplateSandbox.js **/
-jQuery( function ( $ ) {
-	$( '#wpTemplateSandboxPage' ).keypress( function ( ev ) {
-		// On enter keypress, click the TemplateSandbox preview button
-		if ( ev.which === 13 ) {
-			$( '#wpTemplateSandboxPreview' ).click();
-			return false;
-		}
-		return true;
+( function ( $ ) {
+	$( function () {
+		$( '#wpTemplateSandboxPage' ).keypress( function ( e ) {
+			// On enter keypress, click the TemplateSandbox preview button
+			if ( e.which === 13 ) {
+				$( '#wpTemplateSandboxPreview' ).click();
+				return false;
+			}
+			return true;
+		} );
 	} );
-} );
+}( jQuery ) );
