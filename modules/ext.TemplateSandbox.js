3e770281124dbc1e8b997b9c03c32bba22c02527
Ed Sanders <esanders@wikimedia.org>
2018-11-03 12:39:21 +0000

    build: Update devDependencies
     eslint-config-wikimedia      0.5.0  →   0.8.1
     grunt-eslint                20.1.0  →  21.0.0
     grunt-stylelint              0.9.0  →  0.10.1
     stylelint                    8.2.0  →   9.2.0
     stylelint-config-wikimedia   0.4.2  →   0.4.3
    
    Change-Id: Ic43469c7d71af970920d00e0864fc0a503d8e294

--- modules/ext.TemplateSandbox.js
+++ modules/ext.TemplateSandbox.js
@@ -1,11 +1,13 @@
 /** extensions/TemplateSandbox/modules/ext.TemplateSandbox.js **/
-( function ( $, mw ) {
+( function () {
 	$( function () {
 		var widget,
 			$page = $( '#wpTemplateSandboxPage' );
 		if ( $page.hasClass( 'oo-ui-widget' ) ) {
-			// These modules are added to the page from the PHP code, but they may not have loaded yet.
-			// Ideally, they would just be dependencies of this module, but OOUI mode is still optional.
+			// These modules are added to the page from the PHP code,
+			// but they may not have loaded yet.
+			// Ideally, they would just be dependencies of this module,
+			// but OOUI mode is still optional.
 			mw.loader.using( [ 'oojs-ui-core', 'mediawiki.widgets' ] ).done( function () {
 				widget = OO.ui.infuse( $page );
 				widget.on( 'enter', function ( e ) {
@@ -24,4 +26,4 @@
 			} );
 		}
 	} );
-}( jQuery, mediaWiki ) );
+}() );
