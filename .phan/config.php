d15e63072712fa32516cf1912e53b85a6c19b06d
Umherirrender <umherirrender_de.wp@web.de>
2019-10-04 11:23:28 +0200

    phan: Remove suppression for PhanParamTooMany
    Depends-On: I59f2ae1a96af392026fc106e57d23553003c16b8
    Change-Id: I6dbeb1dd22c6d9d9d3a251dc7ae83ab3acae3f41

--- .phan/config.php
+++ .phan/config.php
@@ -4,7 +4,5 @@ $cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.
 
 // ParserOutput->mCustomData
 $cfg['suppress_issue_types'][] = 'PhanUndeclaredProperty';
-// T191666
-$cfg['suppress_issue_types'][] = 'PhanParamTooMany';
 
 return $cfg;
